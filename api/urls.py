a1c205109efdbba4f7ce999c78b2eef19b75fab3
m.tatybaev <m.tatybaev@qiwi.uz>
2022-10-30 01:02:29 +0500

    Clean Django Project created

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,6 @@
 from django.urls import path
-from .views import EntityList, EntityDetail, EntityCreate, EntityFullList, EntityFull2List
 
 
 urlpatterns = [
-    path('entity/<int:pk>/', EntityDetail.as_view()),
-    path('entity/', EntityList.as_view()),
-    path('entity/full/', EntityFullList.as_view()),
-    path('entity/full2/', EntityFull2List.as_view()),
-    path('entity/create/', EntityCreate.as_view()),
+    # path('entity/<int:pk>/', EntityDetail.as_view()),
 ]
